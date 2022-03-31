946ddd63c36b86d6bb5ac2d00056c7ba80211e03
Jenish Hirpara <jenishhirpara23@gmail.com>
2022-03-31 14:23:28 +0530

    detect function

--- src/App.js
+++ src/App.js
@@ -20,6 +20,35 @@ function App() {
   const runBodySegment = async() => {
     const net = await bodyPix.load();
     console.log("BodyPix model loaded");
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  }
+
+  const detect = async(net) => {
+    // Check data is available
+    if(typeof webcamRef.current !== "undefined" &&
+    webcamRef.current !== null &&
+    webcamRef.current.video.readyState === 4) {
+      // Get video properties
+      const video = webcamRef.current.video;
+      const videoHeight = video.videoHeight;
+      const videoWidth = video.videoWidth;
+      
+      // Set video width and height
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width and height
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+      
+      // Make detections
+      const person = await net.segmentPersonParts(video);
+      console.log(person);
+
+      // Draw detections
+    }
   }
 
   runBodySegment();
