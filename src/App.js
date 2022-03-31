b52081f71f251b83426d56e21189491b24809fd6
Jenish Hirpara <jenishhirpara23@gmail.com>
2022-03-31 13:05:18 +0530

    setup webcam and canvas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // 1. Install dependencies DONE
 // 2. Import dependencies DONE
-// 3. Setup webcam and canvas
+// 3. Setup webcam and canvas DONE
 // 4. Define references to those
 // 5. Load bodypix
 // 6. Detect function
@@ -17,18 +17,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480,
+        }} />
+        <canvas ref={canvasRef} style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480,
+        }} />
       </header>
     </div>
   );
