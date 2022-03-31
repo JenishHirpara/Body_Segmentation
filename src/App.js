7e1fad42af3e407dc514c2bc118208676f98fc7a
Jenish Hirpara <jenishhirpara23@gmail.com>
2022-03-31 13:01:13 +0530

    imported dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-// 1. Install dependencies
-// 2. Import dependencies
+// 1. Install dependencies DONE
+// 2. Import dependencies DONE
 // 3. Setup webcam and canvas
 // 4. Define references to those
 // 5. Load bodypix
 // 6. Detect function
 // 7. Draw using drawMask
 
-import logo from './logo.svg';
+import React, {useRef} from "react";
+// import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as bodyPix from "@tensorflow-models/body-pix";
+import Webcam from "react-webcam";
 import './App.css';
 
 function App() {
